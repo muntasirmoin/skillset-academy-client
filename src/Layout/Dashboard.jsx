9848d1caf33a855e2dccd4d5a4edf5a4faede9ca
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-06-10 01:01:02 +0600

    dashboard design start

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
-const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <Outlet></Outlet>
-        </div>
-    );
+const Dashboard = ({ isAdmin, isInstructor }) => {
+  return (
+    <div className="flex">
+      <div className="w-1/4 bg-gray-200">
+        <nav className="p-4">
+          <ul className="space-y-2">
+            {isAdmin && (
+              <>
+                <li>
+                  <NavLink to="/dashboard/admin" activeClassName="font-bold">
+                    Admin Dashboard
+                  </NavLink>
+                </li>
+                {/* Add more admin-specific links */}
+              </>
+            )}
+
+            {isInstructor && (
+              <>
+                <li>
+                  <NavLink to="/dashboard/instructor" activeClassName="font-bold">
+                    Instructor Dashboard
+                  </NavLink>
+                </li>
+                {/* Add more instructor-specific links */}
+              </>
+            )}
+
+            {!isAdmin && !isInstructor && (
+              <>
+                <li>
+                  <NavLink to="/dashboard/student" activeClassName="font-bold">
+                    Student Dashboard
+                  </NavLink>
+                </li>
+                {/* Add more student-specific links */}
+              </>
+            )}
+
+            <li>
+              <NavLink to="/dashboard/home" activeClassName="font-bold">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/profile" activeClassName="font-bold">
+                Profile
+              </NavLink>
+            </li>
+            {/* Add more common links */}
+          </ul>
+        </nav>
+      </div>
+      <div className="w-3/4 bg-white">
+        <header className="p-1 bg-gray-100">
+          {/* Add content for the header section */}
+          <h1 className='text-center'>SkillSet Dashboard</h1>
+        </header>
+      <div className=" bg-white border border-green-300">
+        <Outlet />
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
