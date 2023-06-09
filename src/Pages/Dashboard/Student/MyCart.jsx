16d2b9c4159151262c46e75a9c3435838c3c2957
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-06-10 00:25:07 +0600

    car?.reduce ? added

--- src/Pages/Dashboard/Student/MyCart.jsx
+++ src/Pages/Dashboard/Student/MyCart.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 const MyCart = () => {
     const [cart, refetch] = useCart();
     console.log('cart',cart);
-    const total = cart.reduce((sum, item) => parseFloat(item.price) + sum, 0);
+    const total = cart?.reduce((sum, item) => parseFloat(item.price) + sum, 0);
 
     const handleDelete = (item) =>{
         Swal.fire({
