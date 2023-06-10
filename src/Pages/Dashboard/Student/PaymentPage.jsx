4b2a40aa0dc5c54aed661e9795bfe1943af3598c
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-06-10 12:22:44 +0600

    paymnet stripe 80.4% complete

--- src/Pages/Dashboard/Student/PaymentPage.jsx
+++ src/Pages/Dashboard/Student/PaymentPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+
 
 const PaymentPage = () => {
+
     return (
         <div>
             <h2 className='text-center'>Payment</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
